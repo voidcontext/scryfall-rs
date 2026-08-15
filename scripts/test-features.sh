41a5f061eb0e64f358fcb1b47d2e33bbc3f2cdcc
Gabor Pihaj <gabor.pihaj@gmail.com>
2026-08-15 11:19:47 +0100

    feat: respect Scryfall's rate limits
    This change introduces request throttling, confgurable through rust
    features at compile time.
    
    The test script has been modified to resptect the rate limits.

--- scripts/test-features.sh
+++ scripts/test-features.sh
@@ -1,9 +1,20 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-set -ueo pipefail
+set -xueo pipefail
+
+cmd=${1:-"test"}
+
+targets_except_doc="--lib --bins --examples --tests --benches"
 
 cargo metadata --format-version 1 | jq '.packages[] | select(.name == "scryfall") | .features | keys[]' -r | while read -r feature; do
-    cargo test --no-default-features --features "$feature,default-tls"
+    cargo "$cmd" $targets_except_doc --no-default-features --features "$feature,default-tls"
+    if [ "$cmd" = "test" ]; then 
+        cargo "$cmd" --doc --no-default-features --features "$feature,default-tls,auto_rate_limit_blocking" -- --test-threads 1
+    fi
 done
 
-cargo test --all-features
+cargo "$cmd" $targets_except_doc --all-features
+if [ "$cmd" = "test" ]; then 
+    cargo "$cmd" --doc --all-features  -- --test-threads 1 # auto_rate_limit_blocking is already included
+fi
+
